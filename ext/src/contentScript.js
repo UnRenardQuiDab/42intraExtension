2cb6deb4fa34c81ca5e961c0f88da99d2abe9489
Bastien Wisniewski <bwisniew@student.42lyon.fr>
2024-10-01 17:13:23 +0200

    Add warning tooltip

--- ext/src/contentScript.js
+++ ext/src/contentScript.js
@@ -1,8 +1,9 @@
-import { ConfigProvider } from 'antd';
+import { ConfigProvider, Tooltip } from 'antd';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { UserProvider } from './context/useUser';
 import Calendar from './logtime/Calendar';
+import { WarningOutlined } from '@ant-design/icons';
 
 const browserAPI = process.env.BROWSER === 'firefox' ? browser : chrome;
 
@@ -44,6 +45,19 @@ const App = () => {
 	);
 };
 
+
+const WarningTooltip = () => {
+
+	return(
+		<Tooltip title="Your 42IntraTools token has expired">
+			<WarningOutlined style={{
+				fontSize: '1.5em',
+				marginLeft: '8px',
+			}} />
+		</Tooltip>
+	)
+};
+
 const locationElement = document.getElementById('locations');
 const boxedElement = locationElement ? locationElement.closest('.boxed') : null;
 
@@ -55,19 +69,24 @@ let root = document.createElement('div');
 
 if (boxedElement)
 	{
+		const container = document.querySelectorAll('.container-inner-item');
+		if (container)
+			container.forEach((element) => {
+				element.style.marginLeft = '0'
+			});
 		browserAPI.storage.local.get(['token', 'login', 'maxAge'], function(result) {
 		if (result.token && result.login && new Date(parseInt(result.maxAge)) > Date.now())
-			{
-
-				const container = document.querySelectorAll('.container-inner-item');
-				if (container)
-					container.forEach((element) => {
-						element.style.marginLeft = '0'
-					});
-
-				boxedElement.parentNode.replaceChild(root, boxedElement);
-				ReactDOM.render(<App />, root);
-			}
+		{
+			boxedElement.parentNode.replaceChild(root, boxedElement);
+			ReactDOM.render(<App />, root);
+		}
+		else if (result.token && result.login && new Date(parseInt(result.maxAge)) <= Date.now()){
+			console.warn('Token expired');
+			const title = document.querySelectorAll('.profile-title')[0];
+			title.style.justifyContent = 'flex-start';
+			title.appendChild(root)
+			ReactDOM.render(<WarningTooltip />, root);
+		}
 		});
 	}
 	
